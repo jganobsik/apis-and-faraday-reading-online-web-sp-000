12b6377e04021543715c19196f151cac13549eab
Joe Ganobsik <jganobsik@gmail.com>
2019-05-18 15:05:35 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,6 +9,9 @@ def foursquare
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
     end
+ 
+    body_hash = JSON.parse(@resp.body)
+    @venues = body_hash["response"]["venues"]
     render 'search'
   end
 end
